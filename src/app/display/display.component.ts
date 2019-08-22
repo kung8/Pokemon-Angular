acd7f9c300c8094e7037ae32f88ff7e731609796
Kevin Ung <ung.kevin78@gmail.com>
2019-08-22 08:08:03 -0600

    sorted

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-display',
@@ -9,42 +9,24 @@ import {HttpClient} from '@angular/common/http';
 export class DisplayComponent implements OnInit {
 
   pokemon = [];
-  caught = [];
-
-  constructor(private http: HttpClient) {
-    
-    
-  }
+  constructor(private http: HttpClient) {}
   
   ngOnInit() {
     this.getPokemon()
-    this.handleSort()
-  }
-  
-  
-  getPokemon(){
-    for(let i = 0; i < 152; i++){
-      this.http.get(`https://pokeapi.co/api/v2/pokemon/${i}`).subscribe(res=>{
-        // console.log(res)
-        const {id,name,sprites,types} = res
-        let poke = {
-          id:id,
-          name:name,
-          image: sprites.front_shiny,
-          type:types[0].type.name
-        }
-        this.pokemon.push(poke)
-      })
-    }
-  }
-  
-  
-  async handleSort(){
-    this.pokemon = await this.pokemon.sort((a,b)=> a.id - b.id)
   }
 
-  handleCatch(pokemon){
-    console.log(pokemon)
-    this.caught.push(pokemon)
+  getPokemon =()=> {
+    for (let i = 1; i <=151; i++) {
+      this.http.get(`https://pokeapi.co/api/v2/pokemon/${i}`).subscribe(async res => {
+        const { id, name, sprites, types } = res
+        let poke = {
+          id,
+          name,
+          image: sprites.front_shiny,
+          type: types[0].type.name
+        }
+        this.pokemon = [...this.pokemon,poke].sort((a,b)=> a.id - b.id)
+      })
+    }
   }
 }
